f3a922aa90d3e7e463a254b6f1160bb9a10153f0
Wataru Kurashima <what.r.j150@gmail.com>
2022-02-22 22:37:24 +0900

    feat: 🎸 filename inputable

--- grep-lite/src/main.rs
+++ grep-lite/src/main.rs
@@ -1,3 +1,6 @@
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
 use regex::Regex;
 use clap::{App, Arg};
 
@@ -9,20 +12,22 @@ fn main() {
             .help("The pattern to search for")
             .takes_value(true)
             .required(true))
+        .arg(Arg::with_name("input")
+            .help("File to search")
+            .takes_value(true)
+            .required(true))
         .get_matches();
 
     let pattern = args.value_of("pattern").unwrap();
     let re = Regex::new(pattern).unwrap();
 
-    let quote ="\
-Every face, every shop, bedroom window, public-house, and
-dark square is a picture feverishly turned-- search of what?
-It is the same with books.
-What do we seek through millions of pages?";
+    let input = args.value_of("input").unwrap();
+    let f = File::open(input).unwrap();
+    let reader = BufReader::new(f);
 
-    for line in quote.lines() {
-        let contains_substring = re.find(line);
-        match contains_substring {
+    for line_ in reader.lines() {
+        let line = line_.unwrap();
+        match re.find(&line) {
             Some(_) => println!("{}", line),
             None => (),
         }
