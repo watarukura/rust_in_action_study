8b87389af69b9422d1808b29d958294952ac5229
Wataru Kurashima <what.r.j150@gmail.com>
2022-02-24 19:52:43 +0900

    feat: 🎸 use enum

--- globalerror/src/main.rs
+++ globalerror/src/main.rs
@@ -1,28 +1,63 @@
-use rand::{random};
-
-static mut ERROR: isize = 0;
-
-struct File;
-
-#[allow(unused_variables)]
-fn read(f: &File, save_to: &mut Vec<u8>) -> usize {
-    if random() && random() && random() {
-        unsafe {
-            ERROR = 1;
-        }
-    }
-    0
+#[derive(Debug, PartialEq)]
+enum FileState{
+    Open,
+    Closed,
 }
 
-#[allow(unused_mut)]
-fn main() {
-    let mut f = File;
-    let mut buffer = vec![];
+#[derive(Debug)]
+struct File {
+    name: String,
+    data: Vec<u8>,
+    state: FileState,
+}
 
-    read(&f, &mut buffer);
-    unsafe {
-        if ERROR != 0 {
-            panic!("An error has occured!")
+impl File {
+    fn new(name: &str) -> File {
+        File {
+            name: String::from(name),
+            data: Vec::new(),
+            state: FileState::Closed,
         }
     }
+
+    fn read(self: &File, save_to: &mut Vec<u8>) -> Result<usize, String> {
+        if self.state != FileState::Open {
+            return Err(String::from("File must be open for reading"));
+        }
+        let mut tmp = self.data.clone();
+        let read_length = tmp.len();
+        save_to.reserve(read_length);
+        save_to.append(&mut tmp);
+        Ok(read_length)
+    }
+}
+
+fn open(mut f: File) -> Result<File, String> {
+    f.state = FileState::Open;
+    Ok(f)
+}
+
+fn close(mut f: File) -> Result<File, String> {
+    f.state = FileState::Closed;
+    Ok(f)
+}
+
+fn main() {
+    let mut f5 = File::new("5.txt");
+
+    let mut buffer: Vec<u8> = vec![];
+
+    if f5.read(&mut buffer).is_err() {
+        println!("Error checking is working");
+    }
+
+    f5 = open(f5).unwrap();
+    let f5_length = f5.read(&mut buffer).unwrap();
+    f5 = close(f5).unwrap();
+
+    let text = String::from_utf8_lossy(&buffer);
+
+    println!("{:?}", f5);
+    println!("{} is {} bytes long", &f5.name, f5_length);
+    println!("{}", text);
 }
