faf90a07f34c9939dc9361be1d4120c7b2dfa9af
Wataru Kurashima <what.r.j150@gmail.com>
2022-02-27 01:44:02 +0000

    feat: use clone() or copy()

--- cubesat/src/main.rs
+++ cubesat/src/main.rs
@@ -1,20 +1,8 @@
 #![allow(unused_variables)]
 
-#[derive(Debug)]
+#[derive(Copy,Clone,Debug)]
 struct CubeSat {
     id: u64,
-    mailbox: Mailbox,
-}
-
-impl CubeSat {
-    fn recv(&mut self) -> Option<Message> {
-        self.mailbox.messages.pop()
-    }
-}
-
-#[derive(Debug)]
-enum StatusMessage {
-    Ok,
 }
 
 #[derive(Debug)]
@@ -22,35 +10,86 @@ struct Mailbox {
     messages: Vec<Message>,
 }
 
-type Message = String;
-
-fn check_status(sat_id: CubeSat) -> StatusMessage {
-    StatusMessage::Ok
+#[derive(Debug)]
+struct Message {
+    to: u64,
+    content: String,
 }
 
 struct GroundStation;
 
-impl GroundStation {
-    fn send(&self, to: &mut CubeSat, msg: Message) {
-        to.mailbox.messages.push(msg);
+impl Mailbox {
+    fn post(&mut self, msg: Message) {
+        self.messages.push(msg);
+    }
+
+    fn deliver(&mut self, recipient: &CubeSat) -> Option<Message> {
+        for i in 0..self.messages.len() {
+            if self.messages[i].to == recipient.id {
+                let msg = self.messages.remove(i);
+                return Some(msg);
+            }
+        }
+        None
     }
 }
 
+impl GroundStation {
+    fn connect(&self, sat_id: u64) -> CubeSat {
+        CubeSat { id: sat_id }
+    }
+
+    fn send(&self, mailbox: &mut Mailbox, msg: Message) {
+        mailbox.post(msg);
+    }
+}
+
+impl CubeSat {
+    fn recv(&self, mailbox: &mut Mailbox) -> Option<Message> {
+        mailbox.deliver(&self)
+    }
+}
+
+fn fetch_sat_ids() -> Vec<u64> {
+    vec![1, 2, 3]
+}
+
+#[derive(Copy,Clone,Debug)]
+enum StatusMessage {
+    Ok,
+}
+
+fn check_status(set_id: CubeSat) -> StatusMessage {
+    StatusMessage::Ok
+}
+
 fn main() {
+    let mut mail = Mailbox { messages: vec![] };
     let base = GroundStation {};
+    let sat_ids = fetch_sat_ids();
 
-    let mut sat_a = CubeSat {
-        id: 0,
-        mailbox: Mailbox { messages: vec![] },
-    };
-    println!("t0: {:?}", sat_a);
+    for sat_id in sat_ids {
+        let sat = base.connect(sat_id);
+        let msg = Message {
+            to: sat_id,
+            content: String::from("hello"),
+        };
 
-    base.send(&mut sat_a, Message::from("hello there!"));
+        base.send(&mut mail, msg);
+    }
 
-    println!("t1: {:?}", sat_a);
+    let sat_ids = fetch_sat_ids();
 
-    let msg = sat_a.recv();
-    println!("t2: {:?}", sat_a);
+    for sat_id in sat_ids {
+        let sat = base.connect(sat_id);
+        let msg = sat.recv(&mut mail);
+        println!("{:?}: {:?}", sat, msg);
+    }
 
-    println!("msg: {:?}", msg);
+    let sat_a = CubeSat { id: 0 };
+    let a_status = check_status(sat_a.clone());
+    println!("a: {:?}", a_status.clone());
+
+    let a_status = check_status(sat_a);
+    println!("a: {:?}", a_status);
 }
