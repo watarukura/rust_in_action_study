7c86f005a7cbd6890ea624e80920daa641a33adc
Wataru Kurashima <what.r.j150@gmail.com>
2022-02-27 23:51:31 +0000

    feat: multiplier

--- chip8/src/main.rs
+++ chip8/src/main.rs
@@ -1,48 +1,79 @@
 struct CPU {
-    current_operation: u16,
-    registers: [u8; 2],
+    registers: [u8; 16],
+    position_in_memory: usize,
+    memory: [u8; 0x1000],
 }
 
 impl CPU {
     fn read_opcode(&self) -> u16 {
-        self.current_operation
+        let p = self.position_in_memory;
+        let op_byte1 = self.memory[p] as u16;
+        let op_byte2 = self.memory[p + 1] as u16;
+
+        op_byte1 << 8 | op_byte2
     }
 
     fn run(&mut self) {
-        let opcode = self.read_opcode();
+        loop {
+            let opcode = self.read_opcode();
+            self.position_in_memory += 2;
 
-        let c = ((opcode & 0xF000) >> 12) as u8;
-        let x = ((opcode & 0x0F00) >> 8) as u8;
-        let y = ((opcode & 0x00F0) >> 4) as u8;
-        let d = ((opcode & 0x000F) >> 0) as u8;
+            let c = ((opcode & 0xF000) >> 12) as u8;
+            let x = ((opcode & 0x0F00) >> 8) as u8;
+            let y = ((opcode & 0x00F0) >> 4) as u8;
+            let d = ((opcode & 0x000F) >> 0) as u8;
 
-        // println!("{:?}, {:?}, {:?}, {:?}", &c, &x, &y, &d);
+            // println!("{:?}, {:?}, {:?}, {:?}", &c, &x, &y, &d);
 
-        match (c, x, y, d) {
-            (0x8, _, _, 0x4) => self.add_xy(x, y),
-            _ => todo!("opcode {:04x}", opcode),
+            match (c, x, y, d) {
+                (0, 0, 0, 0) => {
+                    return;
+                }
+                (0x8, _, _, 0x4) => self.add_xy(x, y),
+                _ => todo!("opcode {:04x}", opcode),
+            }
         }
     }
 
     fn add_xy(&mut self, x: u8, y: u8) {
-        self.registers[x as usize] += self.registers[y as usize];
+        let arg1 = self.registers[x as usize];
+        let arg2 = self.registers[y as usize];
+
+        let (val, overflow) = arg1.overflowing_add(arg2);
+        self.registers[x as usize] = val;
+
+        if overflow {
+            self.registers[0xF] = 1;
+        } else {
+            self.registers[0xF] = 0;
+        }
     }
 }
 
 fn main() {
     let mut cpu = CPU {
-        current_operation: 0, // 0: no-op(何もしない)
-        registers: [0; 2],
+        registers: [0; 16],
+        memory: [0; 4096],
+        position_in_memory: 0,
     };
 
-    cpu.current_operation = 0x8014;
     cpu.registers[0] = 5;
     cpu.registers[1] = 10;
+    cpu.registers[2] = 10;
+    cpu.registers[3] = 10;
+
+    let mem = &mut cpu.memory;
+    mem[0] = 0x80;
+    mem[1] = 0x14;
+    mem[2] = 0x80;
+    mem[3] = 0x24;
+    mem[4] = 0x80;
+    mem[5] = 0x34;
 
     cpu.run();
 
-    assert_eq!(cpu.registers[0], 15);
-    println!("5 + 10 = {}", cpu.registers[0]);
+    assert_eq!(cpu.registers[0], 35);
+    println!("5 + 10 + 10 + 10 = {}", cpu.registers[0]);
 
     // let opcode: u16 = 0x71E4;
 
