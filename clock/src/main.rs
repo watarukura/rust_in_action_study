70889b6190f62c2a36cc7f27880457953db0fd83
Wataru Kurashima <what.r.j150@gmail.com>
2022-03-08 23:46:18 +0000

    feat: clock v0.1.1

--- clock/src/main.rs
+++ clock/src/main.rs
@@ -1,6 +1,55 @@
+use chrono::DateTime;
 use chrono::Local;
+use clap::{App, Arg};
+
+struct Clock;
+
+impl Clock {
+    fn get() -> DateTime<Local> {
+        Local::now()
+    }
+
+    fn set() -> ! {
+        unimplemented!()
+    }
+}
 
 fn main() {
-    let now = Local::now();
-    println!("{}", now);
+    let app = App::new("clock")
+        .version("0.1")
+        .about("Gets and (aspirationally) sets the time.")
+        .arg(
+            Arg::with_name("action")
+                .takes_value(true)
+                .possible_values(&["get", "set"])
+                .default_value("get"),
+        )
+        .arg(
+            Arg::with_name("std")
+                .short("s")
+                .long("use-standard")
+                .takes_value(true)
+                .possible_values(&["rfc2822", "rfc3339", "timestamp"])
+                .default_value("rfc3339"),
+        )
+        .arg(Arg::with_name("datetime").help(
+            "When <action> is 'set', apply <datetime>. \
+        Otherwise, ignore.",
+        ));
+
+    let args = app.get_matches();
+    let action = args.value_of("action").unwrap();
+    let std = args.value_of("std").unwrap();
+
+    if action == "set" {
+        unimplemented!()
+    }
+
+    let now = Clock::get();
+    match std {
+        "timestamp" => println!("{}", now.timestamp()),
+        "rfc2822" => println!("{}", now.to_rfc2822()),
+        "rfc3339" => println!("{}", now.to_rfc3339()),
+        _ => unreachable!(),
+    }
 }
